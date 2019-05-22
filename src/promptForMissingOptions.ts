95cc128940327c786795b63faa62b2f0fa9f281f
iwfan <i.wangfancn@gmail.com>
2019-05-22 22:21:42 +0800

    feat: change to typescript

---
+++ src/promptForMissingOptions.ts
@@ -0,0 +1,41 @@
+import inquirer from 'inquirer';
+
+export default async function promptForMissingOptions(options) {
+  const questions = [];
+
+  if (!options.skipPrompts) {
+    questions.push({
+      type: 'confirm',
+      name: 'initGitRepository',
+      message: 'Initialize a git repository?',
+      default: true,
+    });
+  }
+
+  // questions.push({
+  //   type: 'list',
+  //   name: 'language',
+  //   message: 'Please choose which language to use',
+  //   choices: ['TypeScript', 'JavaScript'],
+  //   default: 'TypeScript',
+  // });
+  //
+  // questions.push({
+  //   type: 'list',
+  //   name: 'bundler',
+  //   message: 'Please choose which module bundler to use',
+  //   choices: ['webpack', 'rollup'],
+  //   default: 'webpack',
+  // });
+
+
+  const answers = await inquirer.prompt(questions);
+  return {
+    ...options,
+    initGitRepository: answers.initGitRepository || true,
+    // libraries: {
+    //   language: answers.language,
+    //   bundler: answers.bundler,
+    // }
+  };
+}
